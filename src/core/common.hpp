dee178db99606a96905a97a6e43a0cef4736ce20
ruarq <anton@green-pr.org>
2022-03-28 01:06:16 +0200

    initial commit

---
+++ src/core/common.hpp
@@ -0,0 +1,12 @@
+#ifndef NP_COMMON_HPP
+#define NP_COMMON_HPP
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#define NP_NODISCARD [[nodiscard]]
+
+#endif	  //NP_COMMON_HPP
