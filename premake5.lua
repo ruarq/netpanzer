43f2931d103230040a55129265d343d06ad915c7
ruarq <anton@green-pr.org>
2022-04-12 03:16:55 +0200

    linking SDL2 against np_client

--- premake5.lua
+++ premake5.lua
@@ -1,33 +1,50 @@
+function add_package(pkg)
+	if (pkg == "SDL2") then
+		buildoptions { "`sdl2-config --cflags`" }
+		linkoptions { "`sdl2-config --libs`" }
+	else
+		buildoptions { "`pkg-config " .. pkg .. " --cflags`" }
+		linkoptions { "`pkg-config " .. pkg ..  " --libs`" }
+	end
+end
+
+---------------------------------------------------------------
+
 workspace "netpanzer"
 	configurations { "debug", "release" }
 
--- Global settings
+-- Global settings --------------------------------------------
+
 language "C++"
 cppdialect "C++17"
 
 filter { "configurations:debug" }
-    symbols "On"
-    defines "DEBUG"
+	symbols "On"
+	defines "DEBUG"
 
 filter { "configurations:release" }
-    optimize "Speed"
+	optimize "Speed"
+
+---------------------------------------------------------------
 
 project "np_core"
 	kind "StaticLib"
 	files { "src/core/**.hpp", "src/core/**.cpp" }
 
 project "np_client"
-    kind "ConsoleApp"
+	kind "ConsoleApp"
 
-    files { "src/client/**.hpp", "src/client/**.cpp" }
+	files { "src/client/**.hpp", "src/client/**.cpp" }
 
-    includedirs { "src/core" }
-    links { "np_core" }
+	includedirs { "src/core" }
+	links { "np_core" }
+
+	add_package("SDL2")
 
 project "np_server"
-    kind "ConsoleApp"
+	kind "ConsoleApp"
 
-    files { "src/server/**.hpp", "src/server/**.cpp" }
+	files { "src/server/**.hpp", "src/server/**.cpp" }
 
-    includedirs { "src/core" }
-    links { "np_core" }
+	includedirs { "src/core" }
+	links { "np_core" }
